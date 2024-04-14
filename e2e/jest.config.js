3cf0419f5288f9c43cc09d320dc6dc296e5859ca
Franson33 <antonchikfr@gmail.com>
2024-04-14 15:40:55 +0300

    added possibility to write tests in ts

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
+  preset: 'ts-jest',
   rootDir: '..',
-  testMatch: ['<rootDir>/e2e/**/*.test.tsx'],
+  testMatch: ['<rootDir>/e2e/**/*.test.(js|ts|tsx)'],
   testTimeout: 120000,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
